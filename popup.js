9eca0de0ec6d8e9e123897da93e2ceb38a7c8160
Jake Bergin <mjberg111@gmail.com>
2024-04-08 17:24:13 -0500

    add text size option, partially implement functionality

--- popup.js
+++ popup.js
@@ -1,22 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
     const applyFontButton = document.getElementById('apply-font');
     const fontSelect = document.getElementById('font-select');
+    const fontSize = document.getElementById('size-select');
   
     applyFontButton.addEventListener('click', async function () {
       const selectedFont = fontSelect.value;
+      const selectedSize = fontSize.value;
       try {
         const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
         await chrome.scripting.executeScript({
           target: { tabId: tab.id },
           function: changeFont,
-          args: [selectedFont]
+          args: [selectedFont, selectedSize]
         });
       } catch (error) {
         console.error('Error executing script:', error);
       }
     });
   
-    function changeFont(selectedFont) {
+    function changeFont(selectedFont, selectedSize) {
       document.body.style.fontFamily = selectedFont;
+      document.body.style.fontSize = selectedSize;
     }
   });
