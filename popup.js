59048842b22eb40beee497b3e744c2036bd4704b
kyleblanco <kyleblanco@tamu.edu>
2024-04-16 17:27:59 -0500

    Added the option to change font size

--- popup.js
+++ popup.js
@@ -1,22 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const applyFontButton = document.getElementById('apply-font');
-    const fontSelect = document.getElementById('font-select');
-  
-    applyFontButton.addEventListener('click', async function () {
-      const selectedFont = fontSelect.value;
-      try {
-        const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-        await chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          function: changeFont,
-          args: [selectedFont]
-        });
-      } catch (error) {
-        console.error('Error executing script:', error);
-      }
-    });
-  
-    function changeFont(selectedFont) {
-      document.body.style.fontFamily = selectedFont;
+  const applyFontButton = document.getElementById('apply-font');
+  const fontSelect = document.getElementById('font-select');
+  const textSizeInput = document.getElementById('text-size');
+
+  applyFontButton.addEventListener('click', async function () {
+    const selectedFont = fontSelect.value;
+    const selectedTextSize = textSizeInput.value;
+    try {
+      const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+      await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: changeFont,
+        args: [selectedFont, selectedTextSize]
+      });
+    } catch (error) {
+      console.error('Error executing script:', error);
     }
   });
+
+  function changeFont(selectedFont, selectedTextSize) {
+    document.body.style.fontFamily = selectedFont;
+    document.body.style.fontSize = selectedTextSize + 'px';
+  }
+});
